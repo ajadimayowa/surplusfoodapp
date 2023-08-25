8348da97810ceebf036b648ea31359e39431cca6
ExciteNG <65714464+dolaposokoya@users.noreply.github.com>
2023-08-25 13:06:09 +0100

    Update user.js

---
+++ api/user.js
@@ -0,0 +1,39 @@
+import React from "react";
+import axios from "axios";
+
+
+export const sendList =(userList) => {
+    axios.post('https://surplus-auth-dev-default-rtdb.firebaseio.com/userlist.json'),
+    userList
+
+}
+
+const API_KEY = 'AIzaSyAegImF2nb4zZGAzRJ5BY7D8uMVKD9kv7M';
+
+export const logUserIn = async (userInfo)=>{
+    const body = {
+        ...userInfo,
+        returnSecureToken : true
+    }
+
+    // console.log(body);
+    const res = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`, body);
+    
+    return res;
+
+
+}
+
+export const createUser = async (userInfo)=>{
+    // console.log(userInfo)
+    const body = {
+        ...userInfo,
+        returnSecureToken : true
+    }
+   const res = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`, body);
+   console.log(res);
+   return res;
+   
+   
+}
+
