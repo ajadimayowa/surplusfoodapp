e08c6e6d05a4da823de98226a657084d587bbfc9
ExciteNG <65714464+dolaposokoya@users.noreply.github.com>
2023-08-28 12:08:53 +0100

    added live endpoint and new Profile folder

--- api/host.js
+++ api/host.js
@@ -1,3 +1,4 @@
-export const baseUrl = 'http://192.168.158.30:5100'
+// export const baseUrl = 'http://192.168.158.30:5100'
+export const baseUrl = 'https://surplusfood-9ja.onrender.com'
 
 export const apiUrl = `${baseUrl}/api/v1`
